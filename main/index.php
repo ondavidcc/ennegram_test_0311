e5dfe7aab43a5c74da180777b0ee1df50542c5d9
ondavidcc <38639465+ondavidcc@users.noreply.github.com>
2024-03-11 23:53:50 +0800

    Update index.php

--- main/index.php
+++ main/index.php
@@ -1,10 +1,13 @@
-
-<script src="ennegram_test/main/functions.js"></script>
+<script src="ennegram_test/functions/writeInstruction.js"></script>              
+<script src="ennegram_test/functions/closePopUp.js"></script>
+<script src="ennegram_test/functions/selectLimit.js"></script>
 <script src="ennegram_test/functions/writeCheckbox.js"></script>
- 
-	<div id="content">
-            <div id="description">
+
+        <div id="content">
+            <div id="description" style="text-align: center;">
                 <h1>九型人格卡牌測驗</h1>
+                <p id="instruction"></p>
+                <p id="selectCounter"></p>
             </div>
             <div id="popupBackground">
                 <div id="popupContent">
@@ -12,12 +15,10 @@
                     <button id="closePopupBtn" class="function_button" onclick="closePopup()">我知道了</button>
                 </div>
             </div>
-            <form method="get" id="mainForm" onsubmit="return selectLimit(5);">
+            <form method="get" id="mainForm" onsubmit="return selectLimit(20);">
                 <div id="checkboxContainer"></div>
-                <!-- 考慮使用disabled 標籤 -->
                 <input id="next_page_button" class="function_button" type="submit" value="送出"/>
             </form>
-	</div>
-
+        </div>
 
 <style type="text/css">@import "ennegram_test/styles/mainStyles.css";</style>
