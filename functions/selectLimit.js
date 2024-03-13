cb3391a0a7efff183a2736569a1bd955e2d9e3f9
ondavidcc <38639465+ondavidcc@users.noreply.github.com>
2024-03-13 23:13:47 +0800

    Update selectLimit.js

--- functions/selectLimit.js
+++ functions/selectLimit.js
@@ -3,11 +3,17 @@ function selectLimit(limit){
     const checkedCheckboxes = document.querySelectorAll('.checkbox:checked');
 
     if (checkedCheckboxes.length < limit) {
+        document.getElementById("popupMessage").innerHTML = "您至少需選擇20張卡牌！";
         document.getElementById("popupBackground").style.display = "flex";
         return false;
     }else if (checkedCheckboxes.length == limit){
-        document.getElementById('mainForm').action = "ennegram_test/result/result.html";
+        document.getElementById('mainForm').action = "../result/result.html";
     }else if (checkedCheckboxes.length > limit){
+        if(window.location.href.includes('?')){
+            document.getElementById("popupMessage").innerHTML = "您最多只能選擇20張卡牌！";
+            document.getElementById("popupBackground").style.display = "flex";
+            return false;
+        }
         document.getElementById('mainForm').action = window.location.href;
     }
 }
